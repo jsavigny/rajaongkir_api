660ad134d246fc9b60b5f01d8d9b8109afde5567
jsavigny <jsavigny@gmail.com>
2020-10-05 15:30:15 +0700

    simple docs

--- lib/rajaongkir.rb
+++ lib/rajaongkir.rb
@@ -4,6 +4,10 @@ require 'rajaongkir/version'
 
 module Rajaongkir
   class << self
+
+    # api_key = API Key from RajaOngkir
+    # opts[:host] = Host of RajaOngkir
+    # opts[:type] = Account Type (starter/basic/pro)
     def new(api_key, opts = {})
       Rajaongkir::API.new(api_key, default_options.merge(opts))
     end
